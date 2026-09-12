1c8a5e2692441b95962c2e91c91c0ee214670cac
山下　大星 <KDT25GPR30010@edu.kobe-tech.ac.jp>
2026-09-12 22:13:08 +0900

    リザルトを豪華に

--- EndGamePhase.cpp
+++ EndGamePhase.cpp
@@ -1,4 +1,5 @@
 #include "EndBattlePhase.h"
+#include "Function.h"
 
 PHASE_CONSTRUCTOR(EndBattlePhase)
 {
@@ -27,10 +28,12 @@ void EndBattlePhase::Draw()
 {
 	if (context->isPlayerWin)
 	{
-		DrawCenterText(WINDOW_W / 2, WINDOW_H / 2, "WIN", GetColor(255, 255, 255), 50);
+		DrawGradientRegion(0, WINDOW_H, 255, 210, 60, 20); // –¾‚é‚¢‹àFŒn(j•ŸŠ´)
+		DrawCenterText(WINDOW_W / 2, WINDOW_H / 2, "WIN", GetColor(0, 0, 0), 50);
 	}
 	else
 	{
+		DrawGradientRegion(0, WINDOW_H, 60, 60, 90, 20);   // —‚¿’…‚¢‚½®FŒn(Ã‚©‚È—]‰C)
 		DrawCenterText(WINDOW_W / 2, WINDOW_H / 2, "LOSE", GetColor(255, 255, 255), 50);
 	}
 }
